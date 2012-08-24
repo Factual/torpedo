a4ca03167e0b2e0acd9188625df67b40edb7005b
Spencer Tipping <spencer@spencertipping.com>
2012-08-24 11:11:30 -0700

    Licensing changes for distribution

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
 (defproject torpedo "0.1.0-SNAPSHOT"
   :description "Torpedo complex functional expressions"
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo})
